eb49803404627972e63c85a3e8a72eb364ed4d6f
Fatih Köker <fatih.koker@outlook.com>
2026-04-27 13:11:36 +0200

    Update app 1.2.0

--- app/about.tsx
+++ app/about.tsx
@@ -98,7 +98,7 @@ export default function AboutScreen() {
             resizeMode="contain"
           />
           <Text style={[styles.heroTitle, { color: T.text }]}>Hidayah</Text>
-          <Text style={[styles.heroVersion, { color: T.textMuted }]}>Version 1.1.9</Text>
+          <Text style={[styles.heroVersion, { color: T.textMuted }]}>Version 1.2.0</Text>
         </View>
 
         {/* Sections */}
